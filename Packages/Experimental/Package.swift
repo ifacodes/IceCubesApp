6558309223ae7e56a83069154330cc991b948cff
Aoife Bradley <me@ifa.codes>
2024-01-11 06:28:00 -0500

    Add the Experimental Package to the workspace

---
+++ Packages/Experimental/Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Experimental",
+    defaultLocalization: "en",
+    platforms: [
+      .iOS(.v17),
+      .visionOS(.v1),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Experimental",
+            targets: ["Experimental"]),
+    ],
+    dependencies: [
+      .package(name: "Network", path: "../Network"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Experimental",
+            swiftSettings: [
+              .enableExperimentalFeature("StrictConcurrency"),
+            ]
+        ),
+        .testTarget(
+            name: "ExperimentalTests",
+            dependencies: ["Experimental"]
+        ),
+    ]
+)
